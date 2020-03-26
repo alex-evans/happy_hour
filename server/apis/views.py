7987c4f99e359f8989eda6f6e2413e2053b77edd
Alex Evans <alexevans@Alexs-MacBook-Pro.local>
2020-03-25 21:26:53 -0500

    Bypass csrf check for now

--- server/apis/views.py
+++ server/apis/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from journal.models import Entry
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def apis_view(request):
     if request.POST:
         print("POST")
