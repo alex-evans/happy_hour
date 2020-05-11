fefa8d46940fd5e0596dbdb204275b3d634d2b58
Alex Evans <alexevans@Alexs-MacBook-Pro.local>
2020-05-10 21:45:21 -0500

    Update to use postgresql - started adding actions on the Server Flask side

--- server/database.py
+++ server/database.py
@@ -1,7 +1,9 @@
 
 import os
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-import sqlalchemy as db
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -9,12 +11,41 @@ load_dotenv(dotenv_path)
 DB_USER = os.getenv('DB_USER')
 DB_PW = os.getenv('DB_PW')
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://{DB_USER}:{DB_PW}@localhost:5432/hhdb'
+db = SQLAlchemy(app)
 
-class Database():
-    db_string = f'postgres://{DB_USER}:{DB_PW}@localhost:5432/hhdb'
-    engine = db.create_engine(db_string)
 
-    def __init__(self):
-        self.connection = self.engine.connect()
-        print('DB Instance created')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('lists', lazy=True))
+
+    def __repr__(self):
+        return f'<List {self.name}>'
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(250))
+    completed = db.Column(db.Boolean)
+    due_date = db.Column(db.Date, default=datetime.utcnow)
+
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id'), nullable=False)
+    list = db.relationship('List', backref=db.backref('tasks', lazy=True))
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
--- server/server.py
+++ server/server.py
@@ -1,5 +1,92 @@
 
-import sqlalchemy
+import json
+from flask import request
+from database import app
+from database import User
+from database import List
+# from database import Task
 
 
-db_string = 'postgres://'
+# testing Hardcode user - future pull from a config or force logging it
+USERNAME = 'admin'
+
+
+######################
+# USERS
+######################
+
+
+@app.route('/user/<username>')
+def handle_user(username):
+    user = User.query.filter_by(username=username).first()
+    return f'{user}'
+
+
+######################
+# TASKS
+######################
+
+
+def save_task():
+    return 'TODO - SAVE TASK'
+
+
+def get_tasks():
+    return 'TODO - GET TASKS'
+
+
+@app.route('/tasks', methods=['GET', 'POST'])
+def handle_tasks():
+    if request.method == 'POST':
+        return save_task()
+    else:
+        return get_tasks()
+
+
+######################
+# LISTS
+######################
+
+
+def save_list(req_data):
+    print(req_data)
+    return 'Ok', 200
+
+
+def get_lists():
+    user = User.query.filter_by(username=USERNAME).first()
+    lists = List.query.filter_by(user=user)
+    return {
+        'test': 'lists'
+    }
+
+
+@app.route('/lists', methods=['GET', 'POST'])
+def handle_lists():
+    if request.method == 'POST':
+        return save_list(request.data)
+    else:
+        return get_lists()
+
+
+######################
+# PRINTING
+######################
+
+
+@app.route('/start_day')
+def handle_start_day():
+    return ''
+
+
+@app.route('/')
+def base():
+    return 'Hello - no action taken'
+
+
+######################
+######################
+
+
+if __name__ == '__main__':
+    app.run()
